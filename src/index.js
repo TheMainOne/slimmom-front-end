93833bf961fa4796fbff857943ad9f01a582bdeb
Kostiantyn Ochenash <Ochenashk@gmail.com>
2022-07-08 22:11:59 +0300

    adds fonts fixed after consilium with 4 people

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
