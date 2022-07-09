a5dedbd93f3cb71f3f1756f5e89d1d1e8bcd5ec7
Malogon Anastasua <malogonanastasia@gmail.com>
2022-07-09 18:14:59 +0300

    Update Layout.jsx

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,9 +1,9 @@
 import { LayoutStyles } from './Layout.styled';
-// import Example from 'components/_EXAMPLE';
+import Example from 'components/_EXAMPLE';
 import Container from 'components/Container';
 // import MainPage from 'pages/MainPage/MainPage';
-// import LoginForm from 'components/Forms/LoginForm';
-import LoginPage from 'pages/LoginPage';
+
+
 
 const Layout = () => {
   return (
@@ -12,7 +12,7 @@ const Layout = () => {
       Example компонент, потом возвращать как было */}
       {/* Header */}
       <Container>
-        <LoginPage />
+        <Example />
         {/* Outlet */}
       </Container>
     </LayoutStyles>
