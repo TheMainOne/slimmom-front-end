a8c5085fd0b93c0492c6a430c8a71d141243ed7a
Maksym Lvov <leva880@ukr.net>
2022-07-11 16:58:03 +0300

    fixed margin-top on login page

--- src/pages/LoginPage/LoginPage.styled.jsx
+++ src/pages/LoginPage/LoginPage.styled.jsx
@@ -1,25 +1,28 @@
 import styled from 'styled-components/macro';
 
 const PageContainer = styled.div`
-
-display: flex;
-flex-direction: column; 
-   position:relative;
+  display: flex;
+  flex-direction: column;
+  position: relative;
 `;
 
 const Title = styled.h1`
-font-size: 14px;
-font-weight: 700;
-margin-top: 160px;
-font-family: ${({ theme: { fonts } }) => fonts.gothamPro};
-text-transform: uppercase;
-color: ${({ theme: { colors } }) => colors.$orange};
-text-align: center;
-margin-bottom: 60px;
-@media (min-width: ${({ theme: { breakpoints } }) => breakpoints.tablet}) {
+  font-size: 14px;
+  font-weight: 700;
+  padding-top: 40px;
+  font-family: ${({ theme: { fonts } }) => fonts.gothamPro};
+  text-transform: uppercase;
+  color: ${({ theme: { colors } }) => colors.$orange};
+  text-align: center;
+  margin-bottom: 60px;
+
+  @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.tablet}) {
+    padding-top: 160px;
+  }
+
+  @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.tablet}) {
     text-align: start;
   }
-`
+`;
 
 export { PageContainer, Title };
-
