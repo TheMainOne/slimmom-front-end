5e70a0cc49c6b18217d0ab060bce69be9e8d8bc9
Maksym Lvov <leva880@ukr.net>
2022-07-12 16:36:44 +0300

    some improves

--- src/components/Header/UserInfo/UserInfo.js
+++ src/components/Header/UserInfo/UserInfo.js
@@ -1,5 +1,7 @@
+import useResizeAware from 'react-resize-aware';
 import { useDispatch, useSelector } from 'react-redux';
 import { authOperations } from 'redux/auth';
+import { getName } from 'redux/auth/authSelector';
 
 const styles = {
   container: {
@@ -16,11 +18,16 @@ const styles = {
 };
 
 export default function UserInfo() {
+  const [resizeListener, { width }] = useResizeAware();
   const dispatch = useDispatch();
-  const name = useSelector(state => state.auth.user.name);
+  const name = useSelector(state => getName(state));
+
+  console.log(width, name);
+  console.log(width < 768);
 
   return (
     <div style={styles.container}>
+      {resizeListener}
       <span style={styles.name}>{name}</span>
       <button type="button" onClick={() => dispatch(authOperations.logOut())}>
         Exit
