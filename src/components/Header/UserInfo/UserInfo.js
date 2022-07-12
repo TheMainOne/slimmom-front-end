b3bb336ab73f1d86967ec73b1a312e57c368c5fd
Maksym Lvov <leva880@ukr.net>
2022-07-12 18:04:43 +0300

    Update UserInfo.js

--- src/components/Header/UserInfo/UserInfo.js
+++ src/components/Header/UserInfo/UserInfo.js
@@ -4,27 +4,10 @@ import { authOperations } from 'redux/auth';
 import { getName } from 'redux/auth/authSelector';
 import { HeaderNavButtons, HeaderNavButtonsContainer } from './UserInfo.styled';
 
-const styles = {
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  avatar: {
-    marginRight: 4,
-  },
-  name: {
-    fontWeight: 700,
-    marginRight: 12,
-  },
-};
-
 export default function UserInfo() {
   const [resizeListener, { width }] = useResizeAware();
   const dispatch = useDispatch();
   const name = useSelector(state => getName(state));
-
-  // console.log(width, name);
-  // console.log(width < 768);
 
   return (
     <HeaderNavButtonsContainer>
