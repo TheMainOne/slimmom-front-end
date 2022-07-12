1cb60e3a71f28e11669fb072d14b2839e7237a28
Natatashkin <cs070283sng@gmail.com>
2022-07-12 18:51:24 +0300

    add component DiaryProductList and DiaryProductItem + styles

---
+++ src/components/DiaryProductsList/DiaryProductsList.styled.jsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 306px;
+  margin-top: ${({ theme: { spacing } }) => spacing(8)};
+  padding-bottom: var(--mask-height);
+  font-size: 14px;
+
+  @media (min-width: 768px) {
+    width: 610px;
+    height: 244px;
+    padding-right: 40px;
+    overflow-y: auto;
+
+    ::-webkit-scrollbar {
+      width: 6px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: ${({ theme: { colors } }) => colors.$lightGrey};
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: ${({ theme: { colors } }) => colors.$blue};
+    }
+
+    /* ::-webkit-scrollbar-thumb:hover {
+      background: #555;
+    } */
+
+    //Scrollbar
+    //Scrollbar variables
+    --mask-height: 50px;
+    --padding-right: 40px;
+    --mask-image-content: linear-gradient(
+      to bottom,
+      white var(--mask-height),
+      white calc(100% - var(--mask-height)),
+      transparent
+    );
+    --mask-size-content: calc(100% - var(--scrollbar-width));
+    //end variables
+
+    mask-size: var(--mask-size-content);
+    mask-image: var(--mask-image-content);
+    mask-position: bottom 0, 100% 0;
+    mask-repeat: no-repeat;
+  }
+
+  @media (min-width: 1280px) {
+    width: 623px;
+    height: 271px;
+  }
+`;
+
+export { List };
--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -5,9 +5,11 @@ import { toast } from 'react-toastify';
 // import { DiaryProductsList } from 'components/DiaryProductsList';
 
 import { Spinner } from 'components/Spinner';
-
+import Container from 'components/Container';
 import { DiaryPageStyled } from './DiaryPage.styled';
 import { DiaryDateCalendar } from 'pages/DiaryDateCalendar';
+import DiaryProductsList from 'components/DiaryProductsList';
+import consumedProducts from 'components/DiaryProductsList/products.json';
 
 import { selectActiveDate } from 'redux/slices';
 import { useAddProductMutation } from 'redux/apis';
@@ -42,48 +44,48 @@ const DiaryPage = () => {
   return (
     <PageContainer>
       <DiaryPageStyled>
-        <DiaryDateCalendar />
+        <Container>
+          <DiaryDateCalendar />
 
-        {/* <DiaryAddProductForm /> */}
-        <form
-          onSubmit={onSubmit}
-          style={{ display: 'flex', gap: '32px', flexWrap: 'wrap' }}
-        >
-          <label>
-            <input
-              name="productName"
-              type="text"
-              minLength={3}
-              maxLength={40}
-              required
-            />
-          </label>
+          {/* <DiaryAddProductForm /> */}
+          <form
+            onSubmit={onSubmit}
+            style={{ display: 'flex', gap: '32px', flexWrap: 'wrap' }}
+          >
+            <label>
+              <input
+                name="productName"
+                type="text"
+                minLength={3}
+                maxLength={40}
+                required
+              />
+            </label>
 
-          <label>
-            <input
-              name="weight"
-              type="text"
-              minLength={2}
-              maxLength={6}
-              pattern="\d{2,6}"
-              title="min 2, max 6 digits"
-              required
-            />
-          </label>
+            <label>
+              scrollbar
+              <input
+                name="weight"
+                type="text"
+                minLength={2}
+                maxLength={6}
+                pattern="\d{2,6}"
+                title="min 2, max 6 digits"
+                required
+              />
+            </label>
 
-          <button type="submit" style={{ padding: '10px' }}>
-            [+]
-          </button>
-        </form>
+            <button type="submit" style={{ padding: '10px' }}>
+              [+]
+            </button>
+          </form>
 
-        {isAddingProduct ? (
-          <Spinner />
-        ) : (
-          <>
-            <p>DiaryProductsList</p>
-            {/* <DiaryProductsList items={items} /> */}
-          </>
-        )}
+          {isAddingProduct ? (
+            <Spinner />
+          ) : (
+            <DiaryProductsList consumedProducts={consumedProducts} />
+          )}
+        </Container>
       </DiaryPageStyled>
       <RightSideBar />
     </PageContainer>
