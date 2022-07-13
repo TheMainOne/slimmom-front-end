43f7f41369630ff6ed91c545b1f56b27d0b68cd9
Alex K <alex.workinfo97@gmail.com>
2022-07-13 09:58:41 +0300

    test

--- src/components/RightSideBar/hooks/useTemplate.js
+++ src/components/RightSideBar/hooks/useTemplate.js
@@ -1,35 +1,30 @@
 export const useTemplate = foodListTest => {
-  const list = Object.keys(foodListTest).map(categories => {
-    console.log('~ foodListTest', foodListTest); // зерновые: {products: Array(35), total: 35, totalInBase: 299}, молочные: {products: Array(4), total: 4, totalInBase: 431}
-    console.log('foodListTest[categories]', foodListTest[categories]); //  {products: Array(35), total: 35, totalInBase: 299}
-    console.log('~ categories', categories); // зерновые
-
-    return (
+  const list = Object.keys(foodListTest).map(
+    categories =>
       all(foodListTest[categories], categories) ||
-      some(foodListTest, categories) ||
-      almost(foodListTest, categories) ||
+      some(foodListTest[categories], categories) ||
+      almost(foodListTest[categories], categories) ||
       categories
-    );
-  });
+  );
 
-  console.log('~ list', list);
+  console.log('~ list IN Templalte', list);
+
+  return list;
 };
+// TEMPLATES //
 
 function all({ total, totalInBase }, category) {
   return total === totalInBase ? `Все продукти из групы: ${category}.` : null;
 }
 
 function almost({ total, totalInBase }, category) {
-  console.log('~ total > totalInBase / 2', total > totalInBase / 2);
-
   return total > totalInBase / 2
     ? `Большенство продуктов из групы: ${category}.`
     : null;
 }
 
-function some({ total, products }, category) {
-  console.log('~  total <= 4', total <= 4);
-  const listOfProducts = products.map(pr => pr.title.ru).join(', ');
+function some({ total, products = [] }, category) {
+  const listOfProducts = products?.map(pr => pr.title.ru).join(', ');
   return total <= 4
     ? `Некоторые продукты из групы: ${category} (${listOfProducts}).`
     : null;
--- src/components/RightSideBar/model/FoodList/FoodList.jsx
+++ src/components/RightSideBar/model/FoodList/FoodList.jsx
@@ -5,17 +5,18 @@ import test from './test.json';
 export const FoodList = ({ foodList }) => {
   const foodListTest = test.results.bannedProducts.categories; //test - delete
 
-  const [list] = useTemplate(foodListTest); //change on  foodList
+  const list = useTemplate(foodListTest); //change on  foodList
   console.log('~ list', list);
+
+  const productList = list.map((el, index) => <p key={index}>{el}</p>);
+
+  console.log('~ productList', productList);
 
   const defaultTest = 'Your diet will be displayed here';
 
   return (
     <>
-      <List>
-        {(list.length > 0 && list.map(el => <p key={el}>{el}</p>)) ||
-          defaultTest}
-      </List>
+      <List> {(list.length > 0 && productList) || defaultTest}</List>
     </>
   );
 };
