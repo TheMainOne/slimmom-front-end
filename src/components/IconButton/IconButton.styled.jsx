daea52cb429912a14d937a1a70296b3320aa6f45
Natatashkin <cs070283sng@gmail.com>
2022-07-15 02:16:27 +0300

    create IconButton

---
+++ src/components/IconButton/IconButton.styled.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: flex;
+  margin-bottom: ${({ theme: { spacing } }) => spacing(1)};
+  padding-top: ${({ theme: { spacing } }) => spacing(1)};
+  padding-bottom: ${({ theme: { spacing } }) => spacing(1)};
+  padding-right: ${({ theme: { spacing } }) => spacing(1)};
+  padding-left: ${({ theme: { spacing } }) => spacing(1)};
+  background-color: transparent;
+  border: none;
+  border-radius: 50%;
+  box-shadow: none;
+  outline: none;
+  color: ${({ theme: { colors } }) => colors.$grey};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme: { colors } }) => colors.$lightGrey};
+  }
+
+  &:active {
+    background-color: ${({ theme: { colors } }) => colors.$orange};
+    color: ${({ theme: { colors } }) => colors.$white};
+  }
+
+  & .css-i4bv87-MuiSvgIcon-root {
+    width: 18px;
+    height: 18px;
+  }
+  @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.tablet}) {
+    margin-bottom: ${({ theme: { spacing } }) => spacing(2)};
+    & .css-i4bv87-MuiSvgIcon-root {
+      width: 20px;
+      height: 20px;
+    }
+  }
+`;
+
+export { Button };
