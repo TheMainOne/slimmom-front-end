08da8e531b413db66aa247fa8b95f02b297091dc
Alex K <alex.workinfo97@gmail.com>
2022-07-14 19:29:44 +0300

    style for calc page

--- src/components/CalculatorPageContent/CalculatorPageContent.styled.jsx
+++ src/components/CalculatorPageContent/CalculatorPageContent.styled.jsx
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const CalculatorContainer = styled.div``;
+export const CalculatorContainer = styled.div`
+  margin-top: 32px;
+
+  @media (min-width: 1280px) {
+    margin-top: 62px;
+  }
+`;
