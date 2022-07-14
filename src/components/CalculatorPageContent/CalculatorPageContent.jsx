d7d2cfc7f62b8d42af5dea9ef14408429b487280
Alex K <alex.workinfo97@gmail.com>
2022-07-14 18:13:58 +0300

    add div

--- src/components/CalculatorPageContent/CalculatorPageContent.jsx
+++ src/components/CalculatorPageContent/CalculatorPageContent.jsx
@@ -1,10 +1,13 @@
 import { BlockWrapper } from 'components/Container';
 import CalculatorСalorieForm from 'components/Forms/CalculatorСalorieForm';
 import PageTitle from 'components/PageTitle';
+import { CalculatorContainer } from './CalculatorPageContent.styled';
 
 export const CalculatorPageContent = () => (
   <BlockWrapper>
     <PageTitle title={'Calculate your daily calorie intake right now'} />
-    <CalculatorСalorieForm />
+    <CalculatorContainer>
+      <CalculatorСalorieForm />
+    </CalculatorContainer>
   </BlockWrapper>
 );
--- src/components/CalculatorPageContent/CalculatorPageContent.styled.jsx
+++ src/components/CalculatorPageContent/CalculatorPageContent.styled.jsx
@@ -1,1 +1,3 @@
 import styled from 'styled-components';
+
+export const CalculatorContainer = styled.div``;
