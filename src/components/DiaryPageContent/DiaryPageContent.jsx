cfb1485ba939518460412808d7a3d6404ed84a4f
Natatashkin <cs070283sng@gmail.com>
2022-07-14 11:43:51 +0300

    fix render ProductList

--- src/components/DiaryPageContent/DiaryPageContent.jsx
+++ src/components/DiaryPageContent/DiaryPageContent.jsx
@@ -9,21 +9,21 @@ import React from 'react';
 // import { useAddProductMutation } from 'redux/apis';
 
 import { DiaryAddProductForm } from 'components/Forms/DiaryAddProductForm';
-// import consumedProducts from 'components/DiaryProductsList/products.json';
+
 import {
   DiaryPageContentStyled,
   DiaryPageStyled,
 } from './DiaryPageContent.styled';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export const DiaryPageContent = () => {
-  // const currentDate = useSelector(state => state.calendar.activeDate);
+  const currentDate = useSelector(state => state.calendar.activeDate);
   // const { data: products, isLoading } = useGetProductsQuery();
   // const [addProduct, { isLoading: isAddingProduct }] = useAddProductMutation();
   const [addProduct] = useAddProductMutation();
 
   const { data = {}, isLoading } =
-    diaryApi.useGetProductsByDateQuery('2022-07-13');
+    diaryApi.useGetProductsByDateQuery(currentDate);
 
   const { data: { consumedProducts = [] } = {} } = data;
 
@@ -32,7 +32,7 @@ export const DiaryPageContent = () => {
       <DiaryPageStyled>
         <DiaryDateCalendar />
 
-        <DiaryAddProductForm addProduct={addProduct} />
+        {/* <DiaryAddProductForm addProduct={addProduct} /> */}
       </DiaryPageStyled>
 
       {/* Natasha */}
