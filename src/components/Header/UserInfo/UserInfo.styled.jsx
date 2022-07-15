09186186ffcce76056882522856133c3d8f812e3
Maksym Lvov <leva880@ukr.net>
2022-07-15 19:19:24 +0300

    fixed header styles

--- src/components/Header/UserInfo/UserInfo.styled.jsx
+++ src/components/Header/UserInfo/UserInfo.styled.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import Button from '@mui/material/Button';
 
 export const HeaderNavButtons = styled.span`
   font-family: ${({ theme: { fonts } }) => fonts.gothamPro};
