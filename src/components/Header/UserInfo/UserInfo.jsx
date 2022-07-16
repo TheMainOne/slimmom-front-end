2e8cc0a001aacc5881522b4443a1af4195ed0b93
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-07-16 11:35:43 +0200

    add component modal alert

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -1,22 +1,11 @@
 import useResizeAware from 'react-resize-aware';
-// import { useState } from 'react';
 import { useSelector } from 'react-redux';
-// import { useDispatch } from 'react-redux';
-// import { authOperations } from 'redux/auth';
 import { getName } from 'redux/auth/authSelector';
 import { HeaderNavButtons, HeaderNavButtonsContainer } from './UserInfo.styled';
-import { Alert } from './Alert/Alert';
-// import { Modal } from 'components/Modal';
-// import { ModalContent } from 'components/Modal/ModalContent/ModalContent';
+import { Alert } from './Alert';
 
 export default function UserInfo() {
-  // const [open, setOpen] = useState(false);
-
-  // const handleClickOpen = () => {
-  //   setOpen(true);
-  // };
   const [resizeListener] = useResizeAware();
-  // const dispatch = useDispatch();
   const name = useSelector(state => getName(state));
 
   return (
@@ -25,13 +14,6 @@ export default function UserInfo() {
       <HeaderNavButtons disableRipple color="success">
         {name}
       </HeaderNavButtons>
-      {/* <HeaderNavButtons
-        onClick={() => {
-          dispatch(authOperations.logOut());
-        }}
-      >
-        Exit
-      </HeaderNavButtons> */}
       <Alert />
     </HeaderNavButtonsContainer>
   );
