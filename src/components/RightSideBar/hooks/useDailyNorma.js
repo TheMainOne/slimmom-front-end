cd14cb9f3fd46844ccea0315ab6dc9db46274162
Maksym Lvov <leva880@ukr.net>
2022-07-16 12:10:07 +0300

    fixed no unused vars problem

--- src/components/RightSideBar/hooks/useDailyNorma.js
+++ src/components/RightSideBar/hooks/useDailyNorma.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { userNormaApi } from 'redux/apis';
+// import { userNormaApi } from 'redux/apis';
 import { useGetConsumedByDate } from './useGetConsumedByDate';
 import { useGetUserNorma } from './useGetUserNorma';
 
