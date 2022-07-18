239581dc420ca5609188cc68db87ff9037c6fdc6
Malogon Anastasua <malogonanastasia@gmail.com>
2022-07-18 09:29:20 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect, lazy } from 'react';
+import React, { useEffect, Suspense, lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import { theme, GlobalStyle } from 'styles';
 import { muiTheme } from 'styles';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Layout from 'pages/Layout';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import MainPage from 'pages/MainPage';
-import RegistrationPage from 'pages/RegistrationPage';
+// import MainPage from 'pages/MainPage';
+// import RegistrationPage from 'pages/RegistrationPage';
 import PrivateRoute from '../PrivateRoute';
 import PublicRoute from '../PublicRoute';
-import { authOperations } from 'redux/auth';
+import { authOperations, authSelector } from 'redux/auth';
+import { Spinner } from 'components/Spinner';
+
+const MainPage = lazy(() => import('pages/MainPage'));
+const RegistrationPage = lazy(() => import('pages/RegistrationPage')); 
 const DiaryPage = lazy(() => import('pages/DiaryPage'));
 const CalculatorPage = lazy(() => import('pages/CalculatorPage'));
 const LoginPage = lazy(() => import('pages/LoginPage'));
 
 const App = () => {
   const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelector.getFetchingCurrentUser);
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
@@ -27,6 +32,12 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <MuiThemeProvider theme={muiTheme}>
+        <>
+      {isFetchingCurrentUser ? (
+        <Spinner />
+      ) : (
+        <>
+        {/* <Suspense fallback={<Spinner />}> */}
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route
@@ -72,8 +83,11 @@ const App = () => {
             <Route path="*" element={<Navigate to="/" />} />
           </Route>
         </Routes>
+        {/* </Suspense> */}
+        </>
+        )}
+        </>
       </MuiThemeProvider>
-
       <GlobalStyle />
       <ToastContainer />
     </ThemeProvider>
