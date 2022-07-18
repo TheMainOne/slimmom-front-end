e919c2423197f442ea377095994551fe0ec8f49a
Kostiantyn Ochenash <Ochenashk@gmail.com>
2022-07-18 14:02:14 +0300

    removes draggable modal

--- src/components/MobileModal/MobileModal.js
+++ src/components/MobileModal/MobileModal.js
@@ -1,5 +1,4 @@
 import { useListenEscKeyDown, useToggleNoScroll } from 'hooks/ui';
-import { useCallback, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { getRefs } from 'utils';
 import { MobileModalContent, MobileModalBackdrop } from './MobileModal.styled';
@@ -7,20 +6,11 @@ const { mobileModalRoot } = getRefs();
 
 export const MobileModal = ({ onClose, children }) => {
   useToggleNoScroll();
-  const [isTransparent, setIsTransparent] = useState(false);
-  const onDragToggleBg = useCallback(() => setIsTransparent(show => !show), []);
 
   useListenEscKeyDown(onClose);
 
   return createPortal(
-    <MobileModalBackdrop
-      isTransparent={isTransparent}
-      draggable={true}
-      onTouchEnd={onDragToggleBg}
-      onTouchStart={onDragToggleBg}
-      onDragStart={onDragToggleBg}
-      onDragEnd={onDragToggleBg}
-    >
+    <MobileModalBackdrop>
       <MobileModalContent>{children}</MobileModalContent>
     </MobileModalBackdrop>,
     mobileModalRoot
