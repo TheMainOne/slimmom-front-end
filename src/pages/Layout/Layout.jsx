3b2729a99e70abaf43941e2c931218aea09320a9
Maksym Lvov <leva880@ukr.net>
2022-07-18 11:24:36 +0300

    Update Layout.jsx

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 import Header from 'components/Header/Navigation';
 import BackgroundLayout from 'components/BackgroundLayout';
 import { LayoutStyles } from './Layout.styled';
-import { Spinner } from 'components/Spinner';
 
 const Layout = () => {
   return (
