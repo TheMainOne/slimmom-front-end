589696cf70adde8646f6607624d064371ed8bdf5
Kostiantyn Ochenash <Ochenashk@gmail.com>
2022-07-20 00:49:20 +0300

    closes MobileModal and DesktopModal in one callack

--- src/components/Modal/ModalContent/ModalContent.jsx
+++ src/components/Modal/ModalContent/ModalContent.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from 'components/Button';
 import { DailyCalorieIntake } from 'components/DailyCalorieIntake';
 import { Content, CloseModalButton } from '../Modal.styled';
-export const ModalContent = ({ setShowModal }) => {
+export const ModalContent = ({ handleClose }) => {
   const navigate = useNavigate();
 
   return (
@@ -16,12 +16,7 @@ export const ModalContent = ({ setShowModal }) => {
           onClick={() => navigate('/signup')}
         ></Button>
       </Content>
-      <CloseModalButton
-        aria-label="Close modal"
-        onClick={() => {
-          setShowModal(prev => !prev);
-        }}
-      />
+      <CloseModalButton aria-label="Close modal" onClick={handleClose} />
     </>
   );
 };
