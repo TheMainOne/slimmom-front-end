e37f9e79e7104092f0747914a9b4183fd569813b
TheMainOne <83756212+TheMainOne@users.noreply.github.com>
2022-07-19 12:58:44 +0000

    Deploying to gh-pages from @ TheMainOne/slimmom-front-end@de8aee91bfd7fd422145f9d21a9f8ff2244772c3 🚀

---
+++ static/js/856.f726db7f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkslimmom_front_end=self.webpackChunkslimmom_front_end||[]).push([[856],{6856:function(t,n,a){a.d(n,{FU5:function(){return r}});var e=a(9983);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}}]})(t)}}}]);
+//# sourceMappingURL=856.f726db7f.chunk.js.map
