99898efd829bcc7b33610fe6d646c80e4d0f8edf
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-07-20 16:10:21 +0200

    add translation for modal content

--- src/components/DailyCalorieIntake/List/List.jsx
+++ src/components/DailyCalorieIntake/List/List.jsx
@@ -1,19 +1,26 @@
 import { OlList, Items, ListTitle, ListWrapper } from './List.styled';
 import { useTranslation } from 'react-i18next';
+import { useTemplaitingItem } from '../../RightSideBar/hooks/useTemplaitingItem';
 
-export const List = ({ user, data, isLoading }) => {
+export const List = ({ data }) => {
   const { t } = useTranslation();
+  const categories = useTemplaitingItem(
+    data?.results?.bannedProducts?.categories
+  );
 
-  const categories =
-    data && Object.keys(data?.results?.bannedProducts?.categories);
-
+  const engLanguage = localStorage.getItem('i18nextLng') === 'en';
+  localStorage.getItem('i18nextLtn');
   return (
     <ListWrapper>
       {categories && <ListTitle>{t('bannedFood')}</ListTitle>}
 
       <OlList>
         {categories &&
-          categories.map(product => <Items key={product}>{product}</Items>)}
+          categories?.map((product, index) => (
+            <Items key={index}>
+              {engLanguage ? product.slice(4) : product.slice(26)}
+            </Items>
+          ))}
       </OlList>
     </ListWrapper>
   );
